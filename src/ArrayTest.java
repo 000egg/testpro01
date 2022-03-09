da36ec1c72332ce22945484e833a8757fee4b3d1
home <2907277753@qq.com>
2022-03-09 21:17:35 +0800

    测试

--- src/ArrayTest.java
+++ src/ArrayTest.java
@@ -20,6 +20,8 @@ class Mytools {
                 if (arr[j] > arr[j + 1]) {
                     temp = arr[j];
                     arr[j] = arr[j + 1];
+
+
                     arr[j + 1] = temp;
                 }
             }
